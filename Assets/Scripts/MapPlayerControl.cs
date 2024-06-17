62498a420833292178fc2f2bed759a4b0b420f6f
koro129 <ilhamyumna23@gmail.com>
2024-06-17 18:08:13 +0700

    sak beresin sak

--- Assets/Scripts/MapPlayerControl.cs
+++ Assets/Scripts/MapPlayerControl.cs
@@ -6,11 +6,15 @@ public class MapPlayerControl : MonoBehaviour
 {
     public float movSpeed;
     float speedX, speedY;
+    private Vector3 originalScale;
+    private Animator anim;
     Rigidbody2D rb;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        originalScale = transform.localScale;
     }
 
     void Update()
@@ -18,5 +22,15 @@ public class MapPlayerControl : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.velocity = new Vector2(speedX, speedY);
+        if (speedX < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (speedX > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        
+        anim.SetBool("isRun", speedX != 0);
     }
 }
